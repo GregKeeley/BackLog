3c6c536fe605e3abc4d4b121e60621670bf25531
Gregory Keeley <gregorykeeley@pursuit.org>
2019-11-16 14:23:36 -0500

    Initial Commit

---
+++ BackLog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BackLog
+//
+//  Created by Gregory Keeley on 11/16/19.
+//  Copyright © 2019 Gregory Keeley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

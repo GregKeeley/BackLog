6f478100b6f2b845ae93f0df72f6e5035afb95a0
Gregory Keeley <gregorykeeley@pursuit.org>
2019-11-18 08:11:22 -0500

    -Initiated "GameDetailsViewController"

---
+++ BackLog/GameDetailsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  GameDetailsViewController.swift
+//  BackLog
+//
+//  Created by Gregory Keeley on 11/18/19.
+//  Copyright © 2019 Gregory Keeley. All rights reserved.
+//
+
+import UIKit
+
+class GameDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+  
+    }
+    
+
+}
